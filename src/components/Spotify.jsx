65eb03f13d579b5c0d8e545fc8a0dd87339c3cc0
Ishikajain02 <ishijain02@gmail.com>
2023-05-29 15:53:34 +0530

    basic steps

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './Body'
+import Footer from './Footer'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+
+export default function Spotify() {
+  return (
+    <div>Spotify
+        <Body />
+        <Footer />
+        <Navbar />
+    </div>
+  )
+}
