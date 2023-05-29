33fded0babc574d1957a78866c357fc851be0a82
Ishikajain02 <ishijain02@gmail.com>
2023-05-29 16:02:48 +0530

    layout

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -3,13 +3,52 @@ import Body from './Body'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
-
+import styled from 'styled-components'
 export default function Spotify() {
-  return (
-    <div>Spotify
-        <Body />
-        <Footer />
-        <Navbar />
-    </div>
-  )
-}
+    return (
+        <Container>
+            <div className ="main-body">
+                <Sidebar />
+                <div className='body'>
+                    <Navbar />
+                    <div className='contents'>
+                    <Body/>
+                    </div>
+                </div>
+            </div>
+            <div className='footer'>
+                <Footer/>
+            </div>
+            
+            </Container>
+      )
+    }
+    
+    const Container = styled.div`
+     max-width: 100%;
+      max-height: 200%;
+      overflow: hidden;
+      display: grid;
+      grid-template-rows: 85vh 15vh;
+      .main-body{
+        display: grid;
+        grid-template-columns: 15vw 85vw;
+        height: 100%;
+        width: 100%;
+        background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+        background-color: rgb(32, 87, 100);
+        .body {
+          height: 100%;
+          width: 100%;
+          overflow: auto;
+          &::-webkit-scrollbar {
+            width: 0.7rem;
+            max-height: 2rem;
+            &-thumb {
+              background-color: rgba(255, 255, 255, 0.6);
+            }
+          }
+        }
+      }
+    `;
+    
